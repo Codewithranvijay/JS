495c1eb2833b956c601f5f26c882e5c634cc8177
Ranvijay singh <125644737+Codewithranvijay@users.noreply.github.com>
2024-07-24 02:47:59 +0000

    conversion done

---
+++ 01_Basics/03_conversioAndOperation.js
@@ -0,0 +1,27 @@
+let score = "ranvijay";
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+
+
+//"33"=>33
+//"33abc"=>NaN(not a number), typeof NaN is number
+//true=>1 ,false=>0
+
+
+let isloggedIn=0
+let booleanIsLoggedIn = Boolean(isloggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+// 1=> true ,0=>false
+// ''=>false , 'ranvijay'=>true
+// anynumber => true
+
+let marks= 99 
+
+let studentMarks=String(marks)
+console.log(studentMarks)
+console.log(typeof studentMarks)
